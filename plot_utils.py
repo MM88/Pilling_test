26dfd849988e3d8d23db91d52187fe6b8981558c
Michaela <michaelaservi@gmail.com>
2016-01-26 15:47:33 +0100

    new images

--- plot_utils.py
+++ plot_utils.py
@@ -10,14 +10,14 @@ def plot_trasform_levels(image_wt, nlevels):
     # Show the absolute images for each direction.
     # Note that the 2nd level has index 1 since the 1st has index 0.
     for i in range(nlevels):
-        plt.figure(i)
+        plt.figure('angles'+str(i))
         for slice_idx in range(image_wt.highpasses[i].shape[2]):
             plt.subplot(2, 3, slice_idx)
             plt.imshow(np.angle(image_wt.highpasses[i][:,:,slice_idx]),cmap=plt.cm.gray, clim=(-np.pi, np.pi))
 
     # reconstruction from filters as sum of values in each direction
     for i in range(nlevels):
-        plt.figure(i+nlevels)
+        plt.figure('angle sum' + str(i))
         rec_0 = np.angle(image_wt.highpasses[i][:,:,0])
         for slice_idx in range(image_wt.highpasses[i].shape[2]-1):
             rec_0 = rec_0 + np.angle(image_wt.highpasses[i][:,:,slice_idx+1])
@@ -35,12 +35,20 @@ def plot_approximation_image(image_wt, level):
     # cv2.imshow(window_name,im)
     # cv2.waitKey(0)
 
-    fig = plt.figure()
+    mean = image_wt.scales[level].mean()
+    abs_img = [[(cell- mean) if (cell< mean) else cell for cell in row] for row in image_wt.scales[level] ]
+
+
+    # abs_img = [(row[i]+mean) if (row[i] < mean) else row[i] for i in image.shape[] for row in image ]
+
+    fig = plt.figure('3d level'+ str(level))
     x,y = np.mgrid[:image_wt.scales[level].shape[0],:image_wt.scales[level].shape[1]]
     ax2 = fig.add_subplot(1,1,1,projection='3d')
     ax2.plot_surface(x,y,image_wt.scales[level],cmap=plt.cm.jet,rstride=1,cstride=1,linewidth=0.,antialiased=False)
     ax2.set_title('3D approx '+str(level))
     ax2.set_zlim3d(0,1000)
+
+
 
     return
 
@@ -53,15 +61,33 @@ def plot_reconstructed_detail(image_wt, level):
     py = Pyramid(image_wt.scales[level], yh )
     inv_pill_t = transform.inverse(py)
 
-
     plt.figure( "recon at level " + str(level))
     plt.imshow(inv_pill_t, cmap=plt.cm.gray)
 
+    plt.figure( "details plus approx at level " + str(level))
+    plt.imshow(inv_pill_t + image_wt.scales[level], cmap=plt.cm.gray)
+
+    plt.figure( "details minus approx at level " + str(level))
+    plt.imshow( inv_pill_t - image_wt.scales[level], cmap=plt.cm.gray)
+
+    #
+    # orig = transform.inverse(image_wt)
+    # sub = orig - (inv_pill_t - image_wt.scales[level])
+    # plt.figure( " image minus details minus approx at level " + str(level))
+    # plt.imshow(sub , cmap=plt.cm.gray)
+
     # fig = plt.figure()
-    # x,y = np.mgrid[:inv_pill_t.shape[0],:inv_pill_t.shape[1]]
+    # x,y = np.mgrid[:sub.shape[0],:sub.shape[1]]
     # ax2 = fig.add_subplot(1,1,1,projection='3d')
-    # ax2.plot_surface(x,y,inv_pill_t,cmap=plt.cm.jet,rstride=1,cstride=1,linewidth=0.,antialiased=False)
-    # ax2.set_title('3D recon '+str(level))
+    # ax2.plot_surface(x,y,sub,cmap=plt.cm.jet,rstride=1,cstride=1,linewidth=0.,antialiased=False)
+    # ax2.set_title('3D recon sub'+str(level))
+    # ax2.set_zlim3d(0,1000)
+    #
+    # fig = plt.figure()
+    # x,y = np.mgrid[:orig.shape[0],:orig.shape[1]]
+    # ax2 = fig.add_subplot(1,1,1,projection='3d')
+    # ax2.plot_surface(x,y,orig,cmap=plt.cm.jet,rstride=1,cstride=1,linewidth=0.,antialiased=False)
+    # ax2.set_title('3D recon orig'+str(level))
     # ax2.set_zlim3d(0,1000)
 
 
--- wavelet_trasform.py
+++ wavelet_trasform.py
@@ -18,29 +18,31 @@ def energy_vector(hp):
 
 def main(argv):
 
-
-
     # fig = plt.figure()
     # ax = fig.add_subplot(111, projection='3d')
     # X, Y, Z = axes3d.get_test_data(0.05)
     # ax.plot_wireframe(X, Y, Z, rstride=10, cstride=10)
 
-    # image2 = cv2.imread('bluepill_gray_sqr.jpg')
+    # image2 = cv2.imread('./PILLING/IMG_0822_GS.JPG')
     # gray_image = cv2.cvtColor(image2, cv2.COLOR_BGR2GRAY)
-    # grey = image2[0:image2.shape[0], 0:image2.shape[0]]
-    # cv2.imshow("cc",grey)
-    # cv2.waitKey(0)
-    # cv2.imwrite("./bluepill_gray_sqr.jpg",gray_image)
+    # # grey = image2[0:image2.shape[0], 0:image2.shape[0]]
+    # # cv2.imshow("cc",grey)
+    # # cv2.waitKey(0)
+    # cv2.imwrite("./PILLING/IMG_0822_GS.JPG",gray_image)
 
 
-    image = plt.imread('bluepill_gray_sqr.jpg')
+    # 21/21P, 33, 32, 31, 22
 
+    image = plt.imread('./PILLING/IMG_0822_GS.JPG')
+
+
+    # print np.cov(image)
+    fig = plt.figure( "image")
     # x,y = np.mgrid[:image.shape[0],:image.shape[1]]
-    # ax2 = fig.add_subplot(111,projection='3d')
+    # ax2 = fig.add_subplot(1,1,1,projection='3d')
     # ax2.plot_surface(x,y,image,cmap=plt.cm.jet,rstride=1,cstride=1,linewidth=0.,antialiased=False)
     # ax2.set_title('3D')
     # ax2.set_zlim3d(0,1000)
-
 
     plt.figure("original image")
     plt.imshow(image, cmap = plt.cm.gray)
@@ -49,19 +51,21 @@ def main(argv):
     # cv2.imshow('gray_image',gray_image)
     # cv2.waitKey(0)
     transform = dtcwt.Transform2d()
-    pill_t = transform.forward(image, nlevels=8 ,include_scale=True )
+    pill_t = transform.forward(image, nlevels = 6 ,include_scale=True )
 
     # # draw the approximation at a wanted level
-    plot_utils.plot_approximation_image(pill_t, level = 1)
+    # plot_utils.plot_approximation_image(pill_t, level = 0)
+    plot_utils.plot_approximation_image(pill_t, level = 4)
 
     # # draw the reconstruction at a wanted level, level 0 is the original image
-    plot_utils.plot_reconstructed_detail(pill_t, level = 1)
+    # plot_utils.plot_reconstructed_detail(pill_t, level = 0)
+    plot_utils.plot_reconstructed_detail(pill_t, level = 4)
 
     # # draw each orientation at each trasform level and the resulting image at each level
     # plot_utils.plot_trasform_levels(pill_t, len(pill_t.highpasses))
 
     # #compute energy vector for levels 5 and 6
-    e_vec_mat = []
+    # e_vec_mat = []
     # for i in range(1):
     #      e_vec = energy_vector(pill_t.highpasses[4]) + energy_vector(pill_t.highpasses[5])
     #      e_vec2 = energy_vector(pill_t.highpasses[2]) + energy_vector(pill_t.highpasses[3])
